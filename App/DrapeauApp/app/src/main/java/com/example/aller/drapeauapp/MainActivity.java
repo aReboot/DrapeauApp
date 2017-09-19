9d1b679e3a139f7be15f359784060b05596697de
bigmidlilteam <30695989+bigmidlilteam@users.noreply.github.com>
2017-09-19 09:42:04 +0200

    Commentaire Main Activity

--- App/DrapeauApp/app/src/main/java/com/example/aller/drapeauapp/MainActivity.java
+++ App/DrapeauApp/app/src/main/java/com/example/aller/drapeauapp/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Ajout commentaire Joffrey version 2
-        //ajout commentaire Big
+        //ajout commentaire BigBro
     }
 }
