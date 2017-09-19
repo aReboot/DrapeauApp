3ea9c68726017dfaae9ad285e4d8740bccb7cec9
bigmidlilteam <30695989+bigmidlilteam@users.noreply.github.com>
2017-09-19 14:56:50 +0200

    Tu veux un Thread ?

---
+++ App/DrapeauApp/app/src/main/java/com/example/aller/drapeauapp/thread/TimerHandler.java
@@ -0,0 +1,11 @@
+package com.example.aller.drapeauapp.thread;
+
+/**
+ * Created by Adrien on 19/09/2017.
+ */
+
+public interface TimerHandler {
+
+    public void startTimer();
+    public void stopTimer();
+}
---
+++ App/DrapeauApp/app/src/main/java/com/example/aller/drapeauapp/thread/TimerThread.java
@@ -0,0 +1,12 @@
+package com.example.aller.drapeauapp.thread;
+
+/**
+ * Created by Adrien on 19/09/2017.
+ */
+
+public interface TimerThread extends Runnable {
+
+    public void start();
+    public void stop();
+
+}
