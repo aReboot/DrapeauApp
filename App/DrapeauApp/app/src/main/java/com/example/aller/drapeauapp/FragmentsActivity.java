bffb52c19987be48ec56069c7d052ab8093f41ef
bigmidlilteam <30695989+bigmidlilteam@users.noreply.github.com>
2017-09-19 16:29:01 +0200

    Creation package frargment, plus les deux activités supplementaires ainsi que leurs pages xml.

---
+++ App/DrapeauApp/app/src/main/java/com/example/aller/drapeauapp/FragmentsActivity.java
@@ -0,0 +1,8 @@
+package com.example.aller.drapeauapp;
+
+/**
+ * Created by aller on 19/09/2017.
+ */
+
+public class FragmentsActivity {
+}
---
+++ App/DrapeauApp/app/src/main/java/com/example/aller/drapeauapp/fragments/FragmentsQuizzTexte.java
@@ -0,0 +1,8 @@
+package com.example.aller.drapeauapp.fragments;
+
+/**
+ * Created by aller on 19/09/2017.
+ */
+
+public class FragmentsQuizzTexte {
+}
