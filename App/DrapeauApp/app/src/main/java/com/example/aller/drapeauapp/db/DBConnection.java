1e26928f5fdfd2a87556ccf6a99657b719dca8d4
bigmidlilteam <30695989+bigmidlilteam@users.noreply.github.com>
2017-09-19 13:46:58 +0200

    Commentaire Main Activity

---
+++ App/DrapeauApp/app/src/main/java/com/example/aller/drapeauapp/db/DBConnection.java
@@ -0,0 +1,8 @@
+package com.example.aller.drapeauapp.db;
+
+/**
+ * Created by aller on 19/09/2017.
+ */
+
+public class DBConnection {
+}
---
+++ App/DrapeauApp/app/src/main/java/com/example/aller/drapeauapp/modele/Associer.java
@@ -0,0 +1,8 @@
+package com.example.aller.drapeauapp.modele;
+
+/**
+ * Created by aller on 19/09/2017.
+ */
+
+public class Associer {
+}
