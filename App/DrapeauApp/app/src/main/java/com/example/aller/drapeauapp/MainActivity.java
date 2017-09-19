b5f89dde54f5740315cf6478cdc3c87d92f2baeb
bigmidlilteam <30695989+bigmidlilteam@users.noreply.github.com>
2017-09-19 15:00:50 +0200

    Revert "Tu veux un Thread ?"
    This reverts commit 3ea9c68726017dfaae9ad285e4d8740bccb7cec9.

--- App/DrapeauApp/app/src/main/java/com/example/aller/drapeauapp/MainActivity.java
+++ App/DrapeauApp/app/src/main/java/com/example/aller/drapeauapp/MainActivity.java
@@ -2,30 +2,15 @@ package com.example.aller.drapeauapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ProgressBar;
-
-import com.example.aller.drapeauapp.thread.TimerHandler;
-import com.example.aller.drapeauapp.thread.TimerHandlerImplementation;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void incrementProgressBar() {
-        /*
-        On utilisera ici la méthode incrementProgressBy(1) sur la progressBar
-         */
-    }
-
-    public void resetProgressBar() {
-        /*
-        On utilisera ici la méthode setProgressBar(0) sur la progressBar
-         */
+        //Ajout commentaire Joffrey version 2
+        //ajout commentaire BigBro
+        // nique ta mère
     }
 }
